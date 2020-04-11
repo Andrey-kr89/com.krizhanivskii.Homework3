49d7888bf3f7363dc348f9d5d53ece5084d0d1b3
Andrey-kr89 <andrey.kri89@gmail.com>
2020-04-11 22:06:01 +0300

    all ready

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -13,15 +13,15 @@ public class TaskOne {
             System.out.println("массив должен содержать только числа");
             arrayStringInput = scanner.nextLine();
         }
-        String array1 [] = arrayStringInput.split(" ");
+        String [] array1  = arrayStringInput.split(" ");
 
         int [] arrayM = new int[array1.length];
 
         for (int i = 0; i < array1.length; i++) {
             arrayM[i] = Integer.parseInt(array1[i]);}
 
-        int minValue = 0;
-        int maxValue = 0;
+        int minValue = arrayM[0];
+        int maxValue = arrayM[0];
         int repeatFive = 0;
         for (int i = 0; i < array1.length; i++) {
               if (arrayM[i] < minValue) {minValue = arrayM[i];}
@@ -34,13 +34,48 @@ public class TaskOne {
         if (repeatFive > 0) { System.out.println("Число 5 повторяется " + repeatFive + "раз"); }
         else {System.out.println("Число 5 не встречается"); }
 
-        for (int i = 0; i < arrayM.length; i++) {
-            for (int j = 0; j < arrayM.length; j++){
-                
-            }
 
+        for (int i = 1; i < arrayM.length; i++) {
+            int temp = arrayM[i];
+            int j = i - 1;
+            while(j >= 0 && temp < arrayM[j]) {
+                arrayM[j+1] = arrayM[j];
+                j--;
+            }
+           arrayM[j+1] = temp;
         }
 
+        System.out.println("отсортированный массив:");
+        for (int item : arrayM) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        int countmax = 1;
+        int countmin = 1;
+        int [] arrayR = new int[arrayM.length];
+
+            for (int i = 0; i < arrayM.length; i++) {
+                int count = 0;
+                for (int value : arrayM) {
+                    if (arrayM[i] == value) {
+                        count++;
+                    }
+                    arrayR[i] = count;
+                }
+            }
+
+        for (int value : arrayR) {
+            if (value < countmax) {
+                countmax = value;
+            }
+            if (value > countmin) {
+                countmin = value;
+            }
+        }
+        System.out.println();
+        System.out.println("Максимум повторений чисел в массиве: " + countmax);
+        System.out.println("Минимум повторений чисел в массиве: " + countmin);
 
     }
 
